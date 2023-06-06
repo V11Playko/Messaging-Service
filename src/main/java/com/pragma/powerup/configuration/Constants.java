203e0e6f9a45a688a60d3705e306f69168285166
Heinner Vega <heinnervega20@gmail.com>
2023-06-06 09:51:19 -0500

    HU14 Se añadio una nueva excepcion

---
+++ src/main/java/com/pragma/powerup/configuration/Constants.java
@@ -0,0 +1,10 @@
+package com.pragma.powerup.configuration;
+
+public class Constants {
+    private Constants() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static final String RESPONSE_ERROR_MESSAGE_KEY = "error";
+    public static final String NO_DATA_FOUND_EXCEPTION = "No data found.";
+}
