2f440be78213a90759116b67d4849c4810aef219
Heinner Vega <heinnervega20@gmail.com>
2023-06-06 08:52:03 -0500

    HU14 Se inicio el servicio con lo basico

---
+++ src/main/java/com/pragma/powerup/MessagingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.powerup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingServiceApplication.class, args);
+	}
+
+}
